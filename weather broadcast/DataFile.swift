9f12554d6014a5b51018bcf06e1616c0c48fa77a
Elizabeth <lmazulova@bk.ru>
2024-09-18 23:13:44 +0300

    Все работает!!!

--- weather broadcast/DataFile.swift
+++ weather broadcast/DataFile.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let latitude, longitude, generationtimeMS: Double
+    let utcOffsetSeconds: Int
+    let timezone, timezoneAbbreviation: String
+    let elevation: Int
+    let currentUnits: CurrentUnits
+    let current: Current
+
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude
+        case generationtimeMS = "generationtime_ms"
+        case utcOffsetSeconds = "utc_offset_seconds"
+        case timezone
+        case timezoneAbbreviation = "timezone_abbreviation"
+        case elevation
+        case currentUnits = "current_units"
+        case current
+    }
+}
+
+// MARK: - Current
+struct Current: Codable {
+    let time: String
+    let interval: Int
+    let temperature2M: Double
+
+    enum CodingKeys: String, CodingKey {
+        case time, interval
+        case temperature2M = "temperature_2m"
+    }
+}
+
+// MARK: - CurrentUnits
+struct CurrentUnits: Codable {
+    let time, interval, temperature2M: String
+
+    enum CodingKeys: String, CodingKey {
+        case time, interval
+        case temperature2M = "temperature_2m"
+    }
+}
--- weather broadcast/ViewController.swift
+++ weather broadcast/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WeatherButton: UIButton!
+
+    @IBOutlet weak var WeatherLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+        @IBAction func getweatherbutton(_ sender: Any) {
+            let URLString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m"
+            let url = URL(string: URLString)!
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request){ data, respons, error in
+                if let data = data, let weather = try? JSONDecoder().decode(Welcome.self, from: data){
+                    
+                    DispatchQueue.main.async {
+                        self.WeatherLabel.text = "\(weather.current.temperature2M) °"
+                    }
+                }
+            }
+            task.resume()
+        }
 
 }
 
